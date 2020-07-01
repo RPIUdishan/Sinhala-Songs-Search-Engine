58f364f66ae13a707e2a04ffa1510dc79df0bbfa
RPIUdishan <it18082234@my.sliit.lk>
2020-07-01 19:29:55 +0530

    indexing complete

---
+++ indexing/add_data_to_index.py
@@ -0,0 +1,9 @@
+from elasticsearch import Elasticsearch,helpers
+import json
+
+
+with open('lyrics.json' , encoding='utf-8') as file:
+  objects = json.load(file)
+
+elastic_search  = Elasticsearch()
+helpers.bulk(elastic_search,objects,index="sinhala_songs",doc_type='_doc')
